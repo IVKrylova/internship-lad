d1bbf64e13c0686aa745550bf0ea82e8367eec73
IVKrylova <ivk.lis@yandex.ru>
2023-09-30 23:38:32 +0300

    feat: check authorization

--- layout/LayoutApp/LayoutApp.tsx
+++ layout/LayoutApp/LayoutApp.tsx
@@ -2,10 +2,10 @@ import { FC, ReactNode, useEffect, useState } from "react";
 import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
 
-import { HeaderApp, Footer } from '@components';
+import { HeaderApp, Footer } from "@components";
 import { checkAuth } from "@utils";
 
-import style from './LayoutApp.module.scss';
+import style from "./LayoutApp.module.scss";
 
 type TProps = {
   children: ReactNode;
@@ -15,15 +15,21 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const LayoutApp: FC<TProps> = ({ children }) => {
   const router = useRouter();
+  const { pathname } = router;
 
-  // useEffect(() => {
-  //   const token = localStorage.getItem("token");
-  //   // В коммерческом приложении для проверки автризации нужно
-  //   // отправлять запрос с токеном на сервер, но фейковое API
-  //   // не предоставляет такой возможности. Я создала функцию проверки
-  //   // совпадения токенов, т.к. корректный токен заранее известен
-  //   if (token && checkAuth(token)) router.push("/account");
-  // }, []);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    // В коммерческом приложении для проверки автризации нужно
+    // отправлять запрос с токеном на сервер, но фейковое API
+    // не предоставляет такой возможности. Я создала функцию проверки
+    // совпадения токенов, т.к. корректный токен заранее известен
+    if (
+      (!token && pathname.includes("account")) ||
+      (token && !checkAuth(token) && pathname.includes("account"))
+    ) {
+      router.push("/login");
+    }
+  }, []);
 
   return (
     <div className={`${style.wrap} ${inter.className}`}>
