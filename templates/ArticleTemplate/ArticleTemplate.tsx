97831f7984db952ada171cddcc8c966e3a4b44b6
IVKrylova <ivk.lis@yandex.ru>
2023-10-03 21:28:38 +0300

    style: add media queries

--- templates/ArticleTemplate/ArticleTemplate.tsx
+++ templates/ArticleTemplate/ArticleTemplate.tsx
@@ -11,36 +11,38 @@ type TProps = {
 
 export const ArticleTemplate: FC<TProps> = ({ article }) => {
   return (
-    <>
-      <ButtonGoBack />
-      <div className={style.title}>
-        <ImageWithHideOnError
-          width={400}
-          height={300}
-          alt={`image ${article.title}`}
-          src={article.url}
-          className={style.img}
-        />
-        <H1 title={article.title} className={style.h1} />
-      </div>
-      <p className={style.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
-        dignissimos quaerat, itaque, debitis, hic corporis placeat perspiciatis
-        fuga aut nulla ducimus magni enim cumque! Dolorem recusandae expedita
-        quo optio necessitatibus.
-      </p>
-      <p className={style.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
-        dignissimos quaerat, itaque, debitis, hic corporis placeat perspiciatis
-        fuga aut nulla ducimus magni enim cumque! Dolorem recusandae expedita
-        quo optio necessitatibus.
-      </p>
-      <p className={style.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
-        dignissimos quaerat, itaque, debitis, hic corporis placeat perspiciatis
-        fuga aut nulla ducimus magni enim cumque! Dolorem recusandae expedita
-        quo optio necessitatibus.
-      </p>
-    </>
+    article && (
+      <>
+        <ButtonGoBack />
+        <div className={style.title}>
+          <ImageWithHideOnError
+            width={400}
+            height={200}
+            alt={`image ${article.title}`}
+            src={article.url}
+            className={style.img}
+          />
+          <H1 title={article.title} className={style.h1} />
+        </div>
+        <p className={style.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus dignissimos quaerat, itaque, debitis, hic corporis
+          placeat perspiciatis fuga aut nulla ducimus magni enim cumque! Dolorem
+          recusandae expedita quo optio necessitatibus.
+        </p>
+        <p className={style.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus dignissimos quaerat, itaque, debitis, hic corporis
+          placeat perspiciatis fuga aut nulla ducimus magni enim cumque! Dolorem
+          recusandae expedita quo optio necessitatibus.
+        </p>
+        <p className={style.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus dignissimos quaerat, itaque, debitis, hic corporis
+          placeat perspiciatis fuga aut nulla ducimus magni enim cumque! Dolorem
+          recusandae expedita quo optio necessitatibus.
+        </p>
+      </>
+    )
   );
 };
